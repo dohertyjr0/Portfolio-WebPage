665cdec82bc0e86a7059d22dda7f055f06973f78
doheryjry0 <dohertyjr0@icloud.com>
2024-10-17 21:54:40 +0100

    Added Card

--- Personal-WebPage/src/App.jsx
+++ Personal-WebPage/src/App.jsx
@@ -1,16 +1,13 @@
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
-import Food from './Food.jsx'
+import Card from './Card.jsx'
 
 function App() {
-  
-  return(
-    <>
-      <Header/>
-      <Food/>
-      <Footer/>
-    </>
-  );
+    return(
+      <>
+      <Card/>
+      <Card/>
+      </>
+    );
+
 }
 
 export default App
---
+++ Personal-WebPage/src/Card.jsx
@@ -0,0 +1,13 @@
+
+function Card(){
+
+    return(
+        <div className="card">
+            <img className="card-image" src="https://via.placeholder.com/150" alt="Profile Picture"></img>
+            <h2 className="cardname-title">JohnRoss</h2>
+            <p className="card-text">I study Software Engineering and play video games</p>
+        </div>
+    );
+}
+
+export default Card
